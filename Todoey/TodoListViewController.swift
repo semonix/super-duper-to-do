bb343bf8c3c548e78958bd5f8aef4ca85cef0e96
semonix <44259591+semonix@users.noreply.github.com>
2024-04-29 19:13:24 +0300

    added ["Find Mike", "Buy Eggos", "Destroy Demogogron"].

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -11,13 +11,29 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogogron"]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    //MARK: - Tableview Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        itemArray.count
+    }
     
-
-    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        let message = itemArray[indexPath.row]
+        
+        if #available(iOS 14.0, *) {
+            var content = cell.defaultContentConfiguration()
+            content.text = message
+            cell.contentConfiguration = content
+        } else {
+            cell.textLabel?.text = message
+        }
+        
+        return cell
+    }
 }
-
