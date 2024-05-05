6e40feab00a4813ea2b6a38dac279ee99806ca90
semonix <44259591+semonix@users.noreply.github.com>
2024-05-05 13:43:44 +0300

    renamed the project

--- super-duper to-do/AppDelegate.swift
+++ super-duper to-do/AppDelegate.swift
@@ -16,12 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print(#function)
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        print(#function)
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
